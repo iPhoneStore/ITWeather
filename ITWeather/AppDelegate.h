96bc60686ef59cf6421a869616713a9d52dca3da
Mac <mac@Macs-MacBook-Air.local>
2016-11-16 20:14:31 +0300

    Initial Commit

---
+++ ITWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ITWeather
+//
+//  Created by Mac on 16.11.16.
+//  Copyright © 2016 Ivan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ITWeather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ITWeather
+//
+//  Created by Mac on 16.11.16.
+//  Copyright © 2016 Ivan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
